3b09597c16c18ca75389b9e56a2857d3fec144f1
Alejandro Mantecon Guillen <alemangui@gmail.com>
2014-11-22 00:10:26 +0100

    Uses openmusic-demo-gulp-tasks to perform build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,2 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-
-gulp.task('build', ['build-js', 'build-html']);
-
-gulp.task('build-js', function() {
-	return gulp.src('./demo/main.js')
-		.pipe(browserify({
-			//insertGlobals: false
-		}))
-		.pipe(uglify())
-		.pipe(rename('bundle.js'))
-		.pipe(gulp.dest('./build/'));
-});
-
-gulp.task('build-html', function() {
-	return gulp.src('demo/index.html')
-		.pipe(gulp.dest('./build/'));
-});
-
-gulp.task('watch', function() {
-	gulp.watch(['demo/**/*', 'Oscillator.js'], ['build']);
-});
-
-
-gulp.task('default', ['build', 'watch']);
+require('openmusic-demo-gulp-tasks')(gulp);
